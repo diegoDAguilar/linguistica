b98b1e1d20b52932239957ea12c6724352ccf2b4
diegoDAguilar <diego.deaguilarcanellas@telefonica.com>
2018-01-03 09:30:53 +0100

    Update Clasificador.py

--- Clasificador.py
+++ Clasificador.py
@@ -5,10 +5,6 @@ Created on Tue Jan 02 11:24:21 2018
 @author: Diego-Chop
 """
 import csv
-#INPUT: n documentos, m grupos, los vectores de cada grupo
-
-#Para cada documento, comprobar si cada palabra del texto corresponde a un 
-#vector, si corresponde aniade un contador a ese grupo
 
 
 """
@@ -37,7 +33,7 @@ def readCsv(fileName):
 Reads noticiaPrueba.txt and returns an array d with every word
 """
 def readText():
-    f = open('noticiaPrueba2.txt')
+    f = open('noticiaPrueba3.txt')
     d = []
     for line in f:
         for word in line.split():
@@ -45,61 +41,71 @@ def readText():
     
     return d
 """
-#Arg being the text plus n lists of words
+*Arg being the text plus n lists of words plus the actual lists
+Checks whether a word belongs to a vector of words
+Returns the theme of the article
 """
 def classifier(*arg): 
 
     vectorOfWordsVectors = []
-    cont = 0
-    cont2 = 0
     countHealth = 0
     countSports = 0
+    countSpace = 0
+    machingHealth = []
+    machingSports = []
+    machingSpace = []
     
     #Contains n elements. Each element being a vector of words from the csv    
     for i in range(0, arg[1]):
         vectorOfWordsVectors.append(arg[2 + i])
+        
     #For each word of the text
     for j in arg[0]:
-        #cont += 1
         #For each vector of words from the csv
         for k in range(0, arg[1]):
-            #cont2 += 1
             #For each element of the vector of words from the csv
-            #print len(arg[2 + k])
             for l in range(0, len(arg[2 + k])):
                 if j == arg[2 + k][l][0]:
-                    print 'igual'
                     if k == 0:
                         countHealth += 1
+                        machingHealth.append(j)
                     elif k == 1:
                         countSports += 1
+                        machingSports.append(j)
+                    elif k == 2:
+                        countSpace += 1
+                        machingSpace.append(j)
                     
                     #print j + ' SI es ' + str(arg[2 + k][l][0])
                 else:
-                    print j + ' no es ' + str(arg[2 + k][l][0])
+                    #print j + ' no es ' + str(arg[2 + k][l][0])
                     pass
                     
                     
-    print countSports, countHealth
-    #print cont, cont2
-
+    print countSports, countHealth, countSpace
+    
+    #Prints a message stating the topic.
+    if countSports > max(countHealth, countSpace):
+        print 'El tema de la noticia son los DEPORTES'
+        print machingSports
+    elif countHealth > max(countSports, countSpace):
+        print 'El tema de la noticia es la SALUD'
+        print machingHealth
+    elif countSpace > max(countSports, countHealth):
+        print 'El tema de la noticia es el ESPACIO'
+        print machingSpace
+    else:
+        print 'No se pudo garantizar el tema de la noticia'
     
 def main():
     csvRow, csvWordsVector = readCsv('InformeSalud.csv')
     csvRow2, csvWordsVector2 = readCsv('InformeDeportes.csv')
+    csvRow3, csvWordsVector3 = readCsv('InformeEspacio.csv')
     
-    #print csvWordsVector
-    
-    #print csvRow[5]
-    #print csvWordsVector[5]
-    #print csvWordsVector
+
     d = readText()
-    #print d
-    classifier(d, 2, csvWordsVector, csvWordsVector2 )
+
+    classifier(d, 3, csvWordsVector, csvWordsVector2, csvWordsVector3 )
     
 if __name__ == "__main__":
     main()
-    
-
-
-#OUTPUT: imprimir por pantalla a que m-grupo pertenecen los n documentos.
